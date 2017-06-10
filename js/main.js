55263bb288db846382775006a4e8b5bc042f52de
sashka1096@gmail.com <sashka1096@gmail.com>
2017-06-10 11:37:54 +0300

    Change the way of fixing nav_affix

--- js/main.js
+++ js/main.js
@@ -1,13 +1,26 @@
+// let navAffix = document.getElementsByClassName("header__nav_affix")[0];
+// console.log(navAffix);
+
+// if(navAffix.className.indexOf("header__nav_affix-fixed") === -1) {
+//   var stickyTop = navAffix.offsetTop;
+
+//   document.addEventListener('scroll', function () {
+//     console.log(navAffix.offsetTop);
+//     window.scrollY >= stickyTop ?
+//       navAffix.classList.add('header__nav_affix-fixed') :
+//       navAffix.classList.remove('header__nav_affix-fixed');
+//   });
+// }
 let navAffix = document.getElementsByClassName("header__nav_affix")[0];
-console.log(navAffix);
-
-if(navAffix.className.indexOf("header__nav_affix-fixed") === -1) {
-  var stickyTop = navAffix.offsetTop;
-
-  document.addEventListener('scroll', function () {
-    console.log(navAffix.offsetTop);
-    window.scrollY >= stickyTop ?
-      navAffix.classList.add('header__nav_affix-fixed') :
-      navAffix.classList.remove('header__nav_affix-fixed');
-  });
+let navTop = navAffix.offsetTop;
+window.onscroll = scroll;
+function scroll() {
+  var scrollTop = window.pageYOffset;
+  console.log(scrollTop);
+  if (scrollTop > navTop) {
+    navAffix.classList.add('header__nav_affix-fixed');
+  }
+  else {
+    navAffix.classList.remove('header__nav_affix-fixed');
+  }
 }
