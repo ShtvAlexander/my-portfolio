e7a912c9365989d555a68305bff82788274a7d47
sashka1096@gmail.com <sashka1096@gmail.com>
2017-06-10 12:10:18 +0300

    add affix.js and insert there all JS about nav_affix

---
+++ js/affix.js
@@ -0,0 +1,62 @@
+/*
+ * CSS Affix Navigation 
+ * Put me in a seperate file (affixNav.js)
+ * so any changes can be easily implemented
+ */
+
+//When should I always affix?
+var minWidth = 319;
+//When did I become affixed? [dont edit]
+var stoppedAt = 300;
+//What is the name of the element to affix?
+var navElement = document.getElementsByClassName("header__nav_affix")[0];
+//What class should I give on affix?
+var fixedClass = 'header__nav_affix-fixed'
+
+var navTop = navElement.offsetTop;
+
+//This function turns the element affix
+function initAffix(element) {
+   element.classList.add(fixedClass);
+};
+
+//This delegates the affix based on window width
+//and page scroll position.
+function delegateNav() {
+  if(window.innerWidth > minWidth) {
+    if(window.scrollY > navTop) {
+      initAffix(navElement);
+      stoppedAt = window.scrollY;
+    } else {
+      if(stoppedAt && window.scrollY < stoppedAt) {
+        navElement.classList.remove(fixedClass);
+      }
+    }
+  } else {
+    initAffix(navElement);
+  }
+}
+
+//Check Nav on Page Load
+ready(function() {
+  delegateNav();
+});
+
+//Check Nav on Scroll
+window.onscroll = function() {
+  delegateNav();
+};
+
+//Check Nav on Resize
+window.onresize = function() {
+  navElement.classList.remove(fixedClass);
+  delegateNav();
+};
+
+function ready(fn) {
+  if (document.readyState != 'loading'){
+    fn();
+  } else {
+    document.addEventListener('DOMContentLoaded', fn);
+  }
+}
