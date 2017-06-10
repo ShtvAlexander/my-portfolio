dba585f26307268353f3c813f7a12b6148bd9562
sashka1096@gmail.com <sashka1096@gmail.com>
2017-06-10 10:53:49 +0300

    try to fix nav_affix behavior

--- js/main.js
+++ js/main.js
@@ -1,10 +1,13 @@
 let navAffix = document.getElementsByClassName("header__nav_affix")[0];
-console.log(navAffix.offsetTop);
+console.log(navAffix);
 
-var stickyTop = navAffix.offsetTop;
+if(navAffix.className.indexOf("header__nav_affix-fixed") === -1) {
+  var stickyTop = navAffix.offsetTop;
 
   document.addEventListener('scroll', function () {
+    console.log(navAffix.offsetTop);
     window.scrollY >= stickyTop ?
       navAffix.classList.add('header__nav_affix-fixed') :
       navAffix.classList.remove('header__nav_affix-fixed');
   });
+}
